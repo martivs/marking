ffa2e0a2ad76655011c194bf6d341a89e4f3b201
Mart <m.abramzon@gmail.com>
2024-12-22 07:36:30 +0300

    write bug!

--- main.go
+++ main.go
@@ -18,25 +18,38 @@ func main() {
 	currentTime = strings.ReplaceAll(currentTime, " ", "")
 	currentTime = currentTime[2 : len(currentTime)-2]
 
-	// open file
-	file, err := os.Open("patt.dxf")
+	// open fileR
+	fileR, err := os.Open("patt.dxf")
 	if err != nil {
 		log.Fatal("Error opening file:", err)
 		return
 	}
-	defer file.Close()
+	defer fileR.Close()
+
+	// create fileW
+	fileW, err := os.Create(currentTime + ".dxf")
+	if err != nil {
+		log.Fatal("Error creating file:", err)
+		return
+	}
+	defer fileW.Close()
 
 	// make a scanner
-	scanner := bufio.NewScanner(file)
+	scanner := bufio.NewScanner(fileR)
+
+	// create a writer
+	writer := bufio.NewWriter(fileW)
 
 	// read line by line
-	i := 1
 	for scanner.Scan() {
 		line := scanner.Text()
-		if line == "TEXT" {
-			fmt.Println(i, "TEXT")
-			i++
+
+		_, err := writer.WriteString(line + "\n")
+		if err != nil {
+			os.Remove(currentTime + ".dxf")
+			log.Fatal("Error writing to file:", err)
 		}
+
 	}
 
 	// check for errors
