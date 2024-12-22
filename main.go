40850d0240aa567c590077af5705d3f4b346905b
Mart <m.abramzon@gmail.com>
2024-12-23 02:06:03 +0300

    read block TEXT to []string

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
+	// "fmt"
 	"log"
 	"os"
 	"strings"
@@ -10,39 +11,39 @@ import (
 )
 
 type RW struct {
-	fileR   *os.File
-	fileW   *os.File
-	scanner *bufio.Scanner
-	writer  *bufio.Writer
-	line    string
-	time    string
+	fileR       *os.File
+	fileW       *os.File
+	scanner     *bufio.Scanner
+	writer      *bufio.Writer
+	currentTime string
+	name        string
 }
 
 func main() {
 
 	var rw RW
+	rw.name = "SBS07H"
 
 	// get current time
-	currentTime := time.Now().Format("2006-01-02 15:04:05")
-	currentTime = strings.ReplaceAll(currentTime, "-", "")
-	currentTime = strings.ReplaceAll(currentTime, ":", "")
-	currentTime = strings.ReplaceAll(currentTime, " ", "")
-	currentTime = currentTime[2 : len(currentTime)-2]
+	rw.currentTime = time.Now().Format("2006-01-02 15:04:05")
+	rw.currentTime = strings.ReplaceAll(rw.currentTime, "-", "")
+	rw.currentTime = strings.ReplaceAll(rw.currentTime, ":", "")
+	rw.currentTime = strings.ReplaceAll(rw.currentTime, " ", "")
+	rw.currentTime = rw.currentTime[2 : len(rw.currentTime)-2]
 
 	// open fileR
-
 	fileR, err := os.Open("patt.dxf")
 	if err != nil {
-		log.Fatal("Error opening file:", err)
+		log.Fatal(err)
 		return
 	}
 	rw.fileR = fileR
 	defer fileR.Close()
 
 	// create fileW
-	fileW, err := os.Create(currentTime + ".dxf")
+	fileW, err := os.Create(rw.name + "_" + rw.currentTime + ".dxf")
 	if err != nil {
-		log.Fatal("Error creating file:", err)
+		log.Fatal(err)
 		return
 	}
 	rw.fileW = fileW
@@ -53,32 +54,62 @@ func main() {
 	rw.writer = bufio.NewWriter(fileW)
 
 	// work
-	work(&rw, currentTime)
+	work(&rw)
 }
 
-func work(rw *RW, currentTime string) {
+func work(rw *RW) {
 
 	// read line by line and write to file
 	for rw.scanner.Scan() {
 		line := rw.scanner.Text()
-
-		_, err := rw.writer.WriteString(line + "\n")
-		if err != nil {
-			os.Remove(currentTime + ".dxf")
-			log.Fatal("Error writing to file:", err)
+		if line != "TEXT" {
+			writeLine(rw, line)
+		} else {
+			lines := readTEXT(rw)
+			fmt.Println(lines)
 		}
 	}
 
 	// check for errors
 	if err := rw.scanner.Err(); err != nil {
-		fmt.Println("Error reading file:", err)
+		log.Fatal(err)
 	}
 
-	/*
-		Убедиться, что все данные из буфера записаны в файл
-		+ обработка ошибок для writer.Flush()
-	*/
-	if err := rw.writer.Flush(); err != nil {
-		log.Fatal("Error flushing writer:", err)
+}
+
+func writeLine(rw *RW, line string) {
+
+	_, err := rw.writer.WriteString(line + "\n")
+	if err != nil {
+		log.Fatal(err)
 	}
+
+	// Убедиться, что все данные из буфера записаны в файл
+	// + обработка ошибок для writer.Flush()
+	if err := rw.writer.Flush(); err != nil {
+		log.Fatal(err)
+	}
+
+}
+
+func readTEXT(rw *RW) []string {
+
+	var lines []string
+	lines = append(lines, "TEXT")
+
+	for rw.scanner.Scan() {
+		if rw.scanner.Text() == "  0" {
+			lines = append(lines, "  0")
+			break
+		} else {
+			lines = append(lines, rw.scanner.Text())
+		}
+	}
+
+	// check for errors
+	if err := rw.scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	return lines
 }
