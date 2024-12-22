afb9b567a688f033615446f707cf113d00a66f50
Mart <m.abramzon@gmail.com>
2024-12-22 08:52:47 +0300

    copy decomp.

--- main.go
+++ main.go
@@ -9,7 +9,18 @@ import (
 	"time"
 )
 
+type RW struct {
+	fileR   *os.File
+	fileW   *os.File
+	scanner *bufio.Scanner
+	writer  *bufio.Writer
+	line    string
+	time    string
+}
+
 func main() {
+
+	var rw RW
 
 	// get current time
 	currentTime := time.Now().Format("2006-01-02 15:04:05")
@@ -19,11 +30,13 @@ func main() {
 	currentTime = currentTime[2 : len(currentTime)-2]
 
 	// open fileR
+
 	fileR, err := os.Open("patt.dxf")
 	if err != nil {
 		log.Fatal("Error opening file:", err)
 		return
 	}
+	rw.fileR = fileR
 	defer fileR.Close()
 
 	// create fileW
@@ -32,19 +45,24 @@ func main() {
 		log.Fatal("Error creating file:", err)
 		return
 	}
+	rw.fileW = fileW
 	defer fileW.Close()
 
-	// make a scanner
-	scanner := bufio.NewScanner(fileR)
+	// create scanner & writer
+	rw.scanner = bufio.NewScanner(fileR)
+	rw.writer = bufio.NewWriter(fileW)
 
-	// create a writer
-	writer := bufio.NewWriter(fileW)
+	// work
+	work(&rw, currentTime)
+}
+
+func work(rw *RW, currentTime string) {
 
 	// read line by line and write to file
-	for scanner.Scan() {
-		line := scanner.Text()
+	for rw.scanner.Scan() {
+		line := rw.scanner.Text()
 
-		_, err := writer.WriteString(line + "\n")
+		_, err := rw.writer.WriteString(line + "\n")
 		if err != nil {
 			os.Remove(currentTime + ".dxf")
 			log.Fatal("Error writing to file:", err)
@@ -52,7 +70,7 @@ func main() {
 	}
 
 	// check for errors
-	if err := scanner.Err(); err != nil {
+	if err := rw.scanner.Err(); err != nil {
 		fmt.Println("Error reading file:", err)
 	}
 
@@ -60,8 +78,7 @@ func main() {
 		Убедиться, что все данные из буфера записаны в файл
 		+ обработка ошибок для writer.Flush()
 	*/
-	if err := writer.Flush(); err != nil {
+	if err := rw.writer.Flush(); err != nil {
 		log.Fatal("Error flushing writer:", err)
 	}
-
 }
